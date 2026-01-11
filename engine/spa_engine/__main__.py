ec8d34b28206aca6708854f5d824830b39f60781
Michael Russell <roocell@gmail.com>
2026-01-11 13:06:42 -0500

    trying to depoy

--- engine/spa_engine/__main__.py
+++ engine/spa_engine/__main__.py
@@ -1,8 +1,10 @@
+import os
 import uvicorn
 
 
 def main() -> None:
-    uvicorn.run("spa_engine.api:app", host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("spa_engine.api:app", host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
