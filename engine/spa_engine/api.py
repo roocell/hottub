167f1cc7cf45c24a08b894d9e71a29a939d4f1dc
Michael Russell <roocell@gmail.com>
2026-01-11 21:38:08 -0500

    fixing CORS

--- engine/spa_engine/api.py
+++ engine/spa_engine/api.py
@@ -23,7 +23,11 @@ config = get_config()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://192.168.50.33:3000",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
